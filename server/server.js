03e9bc4b7bdaa16ffafbb097054d81d3363f1a99
hingyimartin <hingyimartin@gmail.com>
2025-06-14 21:04:39 +0200

    Setup server and connect to database

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+
+server.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+server.use(express.json());
+server.use(cookieParser());
+
+// Server
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server running on port: ${process.env.SERVER_PORT}`);
+});
+
+// Database
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+  });
